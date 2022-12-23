03010e0efe3a83c4df91b5392222b58832f7e6d1
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-12-24 03:17:21 +0900

    feat: implement basic post feature

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+import feedparser
+import httpx
+
+SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
+SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
+MASTODON_DOMAIN = os.environ['MASTODON_DOMAIN']
+MASTODON_USERNAME = os.environ['MASTODON_USERNAME']
+
+
+def make_payload(entry, username, icon_url):
+    summary = entry.summary
+
+    media_contents = entry.get('media_content')
+    if media_contents:
+        media_urls = '\n' + '\n'.join(media['url'] for media in media_contents)
+    else:
+        media_urls = ''
+
+    text = f'''{summary}{media_urls}'''
+    return json.dumps({"channel": SLACK_CHANNEL, "username": username, "icon_url": icon_url, "text": text})
+
+
+def post(payload):
+    r = httpx.post(SLACK_WEBHOOK_URL, data=payload)
+    print(r.status_code, r.is_success, r.text)
+
+
+def main():
+    url = f'https://{MASTODON_DOMAIN}/@{MASTODON_USERNAME}.rss'
+    data = feedparser.parse(url)
+
+    user_id = f'{MASTODON_USERNAME}@{MASTODON_USERNAME}'
+    username = f'{data.feed.title} ({user_id})'
+    icon_url = data.feed.webfeeds_icon
+
+    for entry in reversed(data.entries):
+        payload = make_payload(entry, username, icon_url)
+        post(payload)
+        break
+
+
+if __name__ == '__main__':
+    main()
